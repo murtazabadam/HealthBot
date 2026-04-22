db85b37e34f58f53469b8f2d0553c14fed44c14c
Murtaza Badam <murtazabadam@gmail.com>
2026-04-22 15:35:12 +0530

    Connect backend to deployed ML engine

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -5,7 +5,7 @@ const Conversation = require('../models/Conversation');
 
 async function getMLPrediction(symptoms) {
   try {
-    const response = await fetch('http://127.0.0.1:5001/predict', {
+    const response = await fetch('https://tranquil-nourishment-production-11f9.up.railway.app/predict', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ symptoms })
