4d2a436f9fc1a687f7e27834bbed455e3a4bf993
Murtaza Badam <murtazabadam@gmail.com>
2026-04-25 13:57:20 +0530

    Add Google OAuth login

---
+++ backend/config/passport.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/api/auth/google/callback'
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    // Check if user already exists
+    let user = await User.findOne({ googleId: profile.id });
+
+    if (user) return done(null, user);
+
+    // Check if email already registered normally
+    user = await User.findOne({ email: profile.emails[0].value });
+
+    if (user) {
+      // Link Google account to existing user
+      user.googleId = profile.id;
+      user.avatar = profile.photos[0].value;
+      await user.save();
+      return done(null, user);
+    }
+
+    // Create new user
+    user = new User({
+      googleId:  profile.id,
+      name:      profile.displayName,
+      email:     profile.emails[0].value,
+      avatar:    profile.photos[0].value,
+      password:  'google-oauth-no-password'
+    });
+
+    await user.save();
+    return done(null, user);
+  } catch (err) {
+    return done(err, null);
+  }
+}));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+module.exports = passport;
